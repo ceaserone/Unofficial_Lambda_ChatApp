5d1b6bd126d43e400ae6046b2042b22e8213272c
SynAckNetwork <ceaserone@users.noreply.github.com>
2025-08-30 16:13:38 -0400

    Update MainActivity.java

--- app/src/main/java/com/synackdevnet/webcheck/MainActivity.java
+++ app/src/main/java/com/synackdevnet/webcheck/MainActivity.java
@@ -78,6 +78,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        webView.loadUrl("https://lambda.chat");
+        webView.loadUrl("https://127.0.0.1:8080");
     }
 }
